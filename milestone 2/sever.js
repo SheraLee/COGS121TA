c9d9767ee6b32e37080fd507a456716c45acc63f
DVintt <vincealday@gmail.com>
2019-05-01 10:11:41 -0700

    fix

--- milestone 2/sever.js
+++ milestone 2/sever.js
@@ -2,28 +2,30 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('static_files'));
+
 const fakeDatabase = {
     'Philip': {job: 'professor', pet: 'cat.jpg'},
     'John': {job: 'student', pet: 'dog.jpg'},
     'Carol': {job: 'engineer', pet: 'bear.jpg'}
   };
   
-  app.get('/users/', (req, res) => {
+  app.get('/users', (req, res) => {
     const allUsernames = Object.keys(fakeDatabase); // returns a list of object keys
     console.log('allUsernames is:', allUsernames);
     res.send(allUsernames);
   });
   
-  app.get('/users/:userid', (req, res) => {
-    const nameToLookup = req.params.userid; // matches ':userid' above
-    const val = fakeDatabase[nameToLookup];
-    console.log(nameToLookup, '->', val); // for debugging
-    if (val) {
-      res.send(val);
-    } else {
-      res.send({}); // failed, so return an empty object instead of undefined
-    }
-  });
+  //app.get('/users/:userid', (req, res) => {
+    //const nameToLookup = req.params.userid; // matches ':userid' above
+    //const val = fakeDatabase[nameToLookup];
+    //console.log(nameToLookup, '->', val); // for debugging
+    //if (val) {
+      //res.send(val);
+    //} else {
+      //res.send({}); // failed, so return an empty object instead of undefined
+    //}
+  //});
   
   // start the server at URL: http://localhost:3000/
   app.listen(3000, () => {
